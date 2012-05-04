9dc8a069bd2831016e950dddfc32ed2866855cc4
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/PollPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/PollPlugin.pm
+++ lib/TWiki/Plugins/PollPlugin.pm
@@ -12,7 +12,7 @@
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details, published at 
+# GNU General Public License for more details, published at
 # http://www.gnu.org/copyleft/gpl.html
 #
 # =========================
@@ -29,7 +29,7 @@
 #   insidePREHandler     ( $text )
 #   endRenderingHandler  ( $text )
 #
-# initPlugin is required, all other are optional. 
+# initPlugin is required, all other are optional.
 # For increased performance, DISABLE handlers you don't need.
 
 # =========================
@@ -51,27 +51,25 @@ $VERSION = '$Rev$';
 $RELEASE = 'Dakar';
 
 # =========================
-sub initPlugin
-{
+sub initPlugin {
     ( $topic, $web, $user, $installWeb ) = @_;
-    
+
     return 1;
 }
 
 # =========================
-sub commonTagsHandler
-{
+sub commonTagsHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    # collect all votes from the bottom of the topic (after the <!--TWikiPoll--> comment)
-    # just in case if the votes are in the same topic
+# collect all votes from the bottom of the topic (after the <!--TWikiPoll--> comment)
+# just in case if the votes are in the same topic
     my ( undef, undef, $votes ) = split /<\!\-\-TWikiPoll\-\->/, $_[0];
 
     # hide the votes from normal view
-    $_[0] =~ s/<\!\-\-TWikiPoll\-\->.*//gso ;
+    $_[0] =~ s/<\!\-\-TWikiPoll\-\->.*//gso;
 
-    $_[0] =~ s/%POLLRESULTS%/&handlePollResults("",$votes)/geos ;
-    $_[0] =~ s/%POLLRESULTS{(.*?)}%/&handlePollResults($1,$votes)/geos ;
+    $_[0] =~ s/%POLLRESULTS%/&handlePollResults("",$votes)/geos;
+    $_[0] =~ s/%POLLRESULTS{(.*?)}%/&handlePollResults($1,$votes)/geos;
 
 }
 
@@ -80,88 +78,91 @@ sub handlePollResults {
     my ( $attributes, $votes ) = @_;
 
     my %default = (
-        id         => 'id0',                                # id of the poll
-        web        => $web,                                 # web containing the topic
-        topic      => $topic,                               # topic containing the data
-        join       => 'on',                                 # on=join ex-aequo together
-        exaequosep => ', ',                                 # separator between ex-aequo (same-count votes)
-        itemformat => '$item',                              # format of an item
-        countformat=> '$count',                             # format of a count
-        percformat => '$perc%',                             # format of a percentage
-        lineformat => '|  $i{0}  |  $c{0} ($p{0})  |',      # format of a line of the result table
-        limit      => 5,                                    # max number of rows
-        header     => '| *The winners are* | *Votes* |',    # header of the result table
+        id          => 'id0',    # id of the poll
+        web         => $web,     # web containing the topic
+        topic       => $topic,   # topic containing the data
+        join        => 'on',     # on=join ex-aequo together
+        exaequosep  => ', ',     # separator between ex-aequo (same-count votes)
+        itemformat  => '$item',  # format of an item
+        countformat => '$count', # format of a count
+        percformat  => '$perc%', # format of a percentage
+        lineformat => '|  $i{0}  |  $c{0} ($p{0})  |'
+        ,                        # format of a line of the result table
+        limit => 5,              # max number of rows
+        header =>
+          '| *The winners are* | *Votes* |',    # header of the result table
     );
 
     my $params = bless {};
-    
+
     # read defaults from the plugin topic preferences
-    foreach my $k (keys %default) {
-        $params->{$k} = TWiki::Func::extractNameValuePair( $attributes, $k )
-                        ||
-                        TWiki::Func::getPreferencesValue( "POLLPLUGIN_\U$k\E" )
-                        ||
-                        $default{$k};
+    foreach my $k ( keys %default ) {
+        $params->{$k} =
+             TWiki::Func::extractNameValuePair( $attributes, $k )
+          || TWiki::Func::getPreferencesValue("POLLPLUGIN_\U$k\E")
+          || $default{$k};
     }
 
     # get the votes if the topic is not this one
-    if ($web ne $params->{web} || $topic ne $params->{topic} ) {
-	my $text = TWiki::Func::readTopicText( $params->{web}, $params->{topic} ) || "";
-	# collect all votes from the bottom of the topic (after the <!--TWikiPoll--> comment)
-	( undef, undef, $votes ) = split /<\!\-\-TWikiPoll\-\->/, $text;
+    if ( $web ne $params->{web} || $topic ne $params->{topic} ) {
+        my $text =
+          TWiki::Func::readTopicText( $params->{web}, $params->{topic} ) || "";
+
+# collect all votes from the bottom of the topic (after the <!--TWikiPoll--> comment)
+        ( undef, undef, $votes ) = split /<\!\-\-TWikiPoll\-\->/, $text;
     }
 
     # remove empty lines
     $votes =~ s/(\n|\r)+/\n/gso;
-   
-    # list votes 
+
+    # list votes
     my @lines = split /\n/, $votes;
 
     # select the lines with the given id
     # and keep only last user's vote
     my %voted = ();
-    my ($userid, $date, @items);
+    my ( $userid, $date, @items );
     my $id = $params->{id};
     foreach my $line ( grep { /^\s*\|\s*$id\s*\|\s*.*?\s*\|\s*$/ } @lines ) {
-	(undef, undef, $date, $userid, @items) = split /\s*\|\s*/, $line;
-	$voted{$userid} = [@items] ;
+        ( undef, undef, $date, $userid, @items ) = split /\s*\|\s*/, $line;
+        $voted{$userid} = [@items];
     }
 
     # count voted separately for each column (columns 1..)
     # and also collectively (column 0)
     #     * this way you allow for more than 1 preference
-    my @counts  = (); # count for each column and item
-    my @totals  = (); # total for each column
+    my @counts  = ();    # count for each column and item
+    my @totals  = ();    # total for each column
     my $columns = 0;
-    my ($items, $item);
-    foreach $items (values %voted) {
+    my ( $items, $item );
+    foreach $items ( values %voted ) {
         $columns = 1;
         foreach $item ( @{$items} ) {
-            $counts[0]{$item}++;        # cumulative for each item (column 0)
-            $totals[0]++;               # cumulative overall total (column 0)
-            $counts[$columns]{$item}++;  # count the item
-            $totals[$columns]++;         # and the column total
+            $counts[0]{$item}++;           # cumulative for each item (column 0)
+            $totals[0]++;                  # cumulative overall total (column 0)
+            $counts[$columns]{$item}++;    # count the item
+            $totals[$columns]++;           # and the column total
             $columns++;
         }
     }
 
     # rank each column
     my @rank = ();
-    for (my $col=0 ; $col<$columns ; $col++) {
-        $rank[$col] = $params->rankVotes($counts[$col]);
+    for ( my $col = 0 ; $col < $columns ; $col++ ) {
+        $rank[$col] = $params->rankVotes( $counts[$col] );
     }
 
     # generate the table
     my $str = "$params->{header}";
-    my ($col, $lf, $cf, $pf, $perc, $count);
-    for (my $row=0 ; $row < $params->{limit} ; $row++) {
+    my ( $col, $lf, $cf, $pf, $perc, $count );
+    for ( my $row = 0 ; $row < $params->{limit} ; $row++ ) {
         $str .= "\n";
         $lf = "$params->{lineformat}";
-        for ($col=0 ; $col<$columns ; $col++) {
-            if (defined($rank[$col][$row])) {
-                ($count, $items) = @{$rank[$col][$row]};
-                $items = join($params->{exaequosep}, @$items);
-                $perc = sprintf("%.2d",$count*100/$totals[$col]);
+        for ( $col = 0 ; $col < $columns ; $col++ ) {
+            if ( defined( $rank[$col][$row] ) ) {
+                ( $count, $items ) = @{ $rank[$col][$row] };
+                $items = join( $params->{exaequosep}, @$items );
+                $perc = sprintf( "%.2d", $count * 100 / $totals[$col] );
                 $pf = "$params->{percformat}";
                 $pf =~ s/\$perc/$perc/g;
                 $cf = "$params->{countformat}";
@@ -175,41 +176,51 @@ sub handlePollResults {
         $lf =~ s/\$c\{.*?\}//g;
         $lf =~ s/\$p\{.*?\}//g;
         $str .= "$lf";
-    }        
+    }
 
     return $str;
 }
 
 # =========================
-# 
+#
 sub rankVotes ($$) {
-    my ($self, $c) = @_;
-    my %counts = %$c;        # column of values to be counted
-    my @column = ();         # resulting list of pairs [count,item]
+    my ( $self, $c ) = @_;
+    my %counts = %$c;    # column of values to be counted
+    my @column = ();     # resulting list of pairs [count,item]
 
-    my ($item, $count, $f, %exaequo);
+    my ( $item, $count, $f, %exaequo );
+
     # join ex-aequo elements if required
-    if ($self->{join} eq 'on') {
+    if ( $self->{join} eq 'on' ) {
+
         # we want to collect all the ex-equo items together
-        while ( ($item, $count) = each %counts) {
+        while ( ( $item, $count ) = each %counts ) {
             $f = $self->{itemformat};
             $f =~ s/\$item/$item/g;
-            if ( ! defined( $exaequo{$count} ) ) {
+            if ( !defined( $exaequo{$count} ) ) {
                 $exaequo{$count} = [];
             }
-            push @{$exaequo{$count}}, ($f);
+            push @{ $exaequo{$count} }, ($f);
         }
+
         # then sort the values
         my $i = 0;
-        foreach $count ( reverse sort {$a <=> $b} keys %exaequo ) {
-            $column[$i++] = [$count,$exaequo{$count}];
+        foreach $count ( reverse sort { $a <=> $b } keys %exaequo ) {
+            $column[ $i++ ] = [ $count, $exaequo{$count} ];
         }
-    } else {
+    }
+    else {
+
         # we want just the values in reverse count order
         my $i = 0;
+
         # sort on the values
-        foreach $item ( reverse sort {$counts{$a} <=> $counts{$b}} keys %counts ) {
-            $column[$i] = [$counts{$item},[$item]];
+        foreach $item (
+            reverse sort { $counts{$a} <=> $counts{$b} }
+            keys %counts
+          )
+        {
+            $column[$i] = [ $counts{$item}, [$item] ];
             $i++;
         }
     }
